b629a080430d85239b8f7c12512da6f6d71b5074
SSE4 <tomskside@gmail.com>
2017-11-19 22:46:52 +0700

    - workaround for MSVC shared build

--- conanfile.py
+++ conanfile.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from conans import ConanFile, CMake, tools
+import os
 
 
 class LibtiffConan(ConanFile):
@@ -23,14 +24,20 @@ class LibtiffConan(ConanFile):
 
     def build(self):
         cmake = CMake(self)
-        cmake.definitions["lzma"] = "OFF"
-        cmake.definitions["jpeg"] = "OFF"
+        cmake.definitions["lzma"] = False
+        cmake.definitions["jpeg"] = False
+        if self.options.shared and self.settings.compiler == "Visual Studio":
+            # https://github.com/Microsoft/vcpkg/blob/master/ports/tiff/fix-cxx-shared-libs.patch
+            tools.replace_in_file(os.path.join('tiff-4.0.8', 'libtiff', 'CMakeLists.txt'),
+                                  r'set_target_properties(tiffxx PROPERTIES SOVERSION ${SO_COMPATVERSION})',
+                                  r'set_target_properties(tiffxx PROPERTIES SOVERSION ${SO_COMPATVERSION} '
+                                  r'WINDOWS_EXPORT_ALL_SYMBOLS ON)')
         if self.settings.os == "Linux":
             cmake.definitions["CMAKE_POSITION_INDEPENDENT_CODE"] = "ON"
         cmake.definitions["BUILD_SHARED_LIBS"] = self.options.shared
         cmake.configure(build_dir="build")
         cmake.build(target="install")
-        
+
     def package(self):
         self.copy("*.h", dst="include", src="sources")
 
