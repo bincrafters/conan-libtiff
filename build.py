014d8de8ab11c89cc1f88e00afa8cc31b8140074
theirix <theirix@gmail.com>
2018-02-03 09:11:16 +0300

    Skip mingw cross-builds

--- build.py
+++ build.py
@@ -8,4 +8,12 @@ if __name__ == "__main__":
 
     builder = build_template_default.get_builder()
 
+    items = []
+    for item in builder.items:
+        # skip mingw cross-builds
+        if not (platform.system() == "Windows" and item.settings["compiler"] == "gcc" and
+                item.settings["arch"] == "x86"):
+            items.append(item)
+    builder.items = items
+
     builder.run()
