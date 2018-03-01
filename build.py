2d9d8553acb281424a73706f1d0030caa2d7f727
SSE4 <tomskside@gmail.com>
2018-03-01 23:18:37 +0700

    - libtiff is pure C

--- build.py
+++ build.py
@@ -7,7 +7,7 @@ import platform
 
 if __name__ == "__main__":
 
-    builder = build_template_default.get_builder()
+    builder = build_template_default.get_builder(pure_c=True)
 
     items = []
     for item in builder.items:
